c870056c890f7ce8ece6013c37490e85d236dfab
ezratameno <ezratameno@gmail.com>
2023-06-30 14:34:12 +0300

    ep15 - Refactoring Part 1/3

---
+++ app/services/currency/data/rates.go
@@ -0,0 +1,68 @@
+package data
+
+import (
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/hashicorp/go-hclog"
+)
+
+type ExchangeRates struct {
+	log   hclog.Logger
+	rates map[string]float64
+}
+
+func NewRates(l hclog.Logger) (*ExchangeRates, error) {
+	er := &ExchangeRates{
+		log:   l,
+		rates: make(map[string]float64),
+	}
+
+	err := er.getExchangeRates()
+	if err != nil {
+		return nil, err
+	}
+
+	return er, nil
+}
+
+type Cubes struct {
+	CubeData []Cube `xml:"Cube>Cube>Cube"`
+}
+
+type Cube struct {
+	Currency string `xml:"currency,attr"`
+	Rate     string `xml:"rate,attr"`
+}
+
+// getExchangeRates will get the exchange rates from the central bank.
+func (e *ExchangeRates) getExchangeRates() error {
+
+	resp, err := http.DefaultClient.Get("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("status code %d", resp.StatusCode)
+	}
+
+	md := &Cubes{}
+
+	xml.NewDecoder(resp.Body).Decode(&md)
+
+	for _, c := range md.CubeData {
+		r, err := strconv.ParseFloat(c.Rate, 64)
+		if err != nil {
+			return err
+		}
+
+		e.rates[c.Currency] = r
+	}
+
+	return nil
+
+}
