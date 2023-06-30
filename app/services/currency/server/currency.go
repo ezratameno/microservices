cbbb282917ab6cf850e7ced110f424f511d65159
ezratameno <ezratameno@gmail.com>
2023-06-30 19:41:12 +0300

    ep16 - RPC Bi-directional streaming, part 1/2
    gRPC streaming allows you to independently receive streamed messages from the client and send a response to it. This episode looks at the basics of streaming API by adding an update to our currency service.

--- app/services/currency/server/currency.go
+++ app/services/currency/server/currency.go
@@ -3,6 +3,8 @@ package server
 import (
 	"context"
 	"fmt"
+	"io"
+	"time"
 
 	"github.com/ezratameno/microservices/app/services/currency/data"
 	currency "github.com/ezratameno/microservices/app/services/currency/protos/currency/app/services/currency/protos"
@@ -39,6 +41,39 @@ func (c *Currency) GetRate(ctx context.Context, request *currency.RateRequest) (
 
 	fmt.Println(rate)
 	return &currency.RateResponse{Rate: rate}, nil
+}
+
+func (c *Currency) SubscribeRates(src currency.Currency_SubscribeRatesServer) error {
+
+	go func() {
+		for {
+			// Recv is blocking
+			rr, err := src.Recv()
+			if err == io.EOF {
+				c.log.Info("client has closed connection")
+				break
+			}
+			if err != nil {
+				c.log.Error("unable to read from client")
+				break
+			}
+
+			c.log.Info("handle client request", rr)
+		}
+	}()
+
+	// send updates all the times.
+	for {
+
+		// update rates.
+		err := src.Send(&currency.RateResponse{Rate: 12.1})
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+
 }
 
 // mustEmbedUnimplementedCurrencyServer()
